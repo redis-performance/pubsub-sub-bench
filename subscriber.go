54fbb9f28729a45f9fe5b6953d0f7f39fd5ee5b7
filipecosta90 <filipecosta.90@gmail.com>
2023-06-15 13:02:17 +0100

    Removed radix usage from --oss-cluster-api-distribute-subscribers. Using clusterClient.MasterForKey() in case of cluster API and SSUBSCRIBE

--- subscriber.go
+++ subscriber.go
@@ -5,8 +5,6 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	radix "github.com/mediocregopher/radix/v4"
-	"github.com/redis/go-redis/v9"
 	"io/ioutil"
 	"log"
 	"os"
@@ -17,6 +15,17 @@ import (
 	"sync/atomic"
 	"text/tabwriter"
 	"time"
+
+	redis "github.com/redis/go-redis/v9"
+)
+
+const (
+	redisPoolSize              = "pool_size"
+	redisPoolSizeDefault       = 0
+	redisTLSCA                 = "tls_ca"
+	redisTLSCert               = "tls_cert"
+	redisTLSKey                = "tls_key"
+	redisTLSInsecureSkipVerify = "tls_insecure_skip_verify"
 )
 
 var totalMessages uint64
@@ -37,15 +46,9 @@ type testResult struct {
 	Addresses             []string  `json:"Addresses"`
 }
 
-func subscriberRoutine(addr string, mode, subscriberName string, channel string, printMessages bool, ctx context.Context, wg *sync.WaitGroup, opts radix.Dialer, protocolVersion int) {
+func subscriberRoutine(mode, channel string, printMessages bool, ctx context.Context, wg *sync.WaitGroup, client *redis.Client) {
 	// tell the caller we've stopped
 	defer wg.Done()
-	client := redis.NewClient(&redis.Options{
-		Addr:            addr,
-		Password:        opts.AuthPass,
-		ClientName:      subscriberName,
-		ProtocolVersion: protocolVersion,
-	})
 	switch mode {
 	case "ssubscribe":
 		spubsub := client.SSubscribe(ctx, channel)
@@ -99,42 +102,119 @@ func main() {
 	client_output_buffer_limit_pubsub := flag.String("client-output-buffer-limit-pubsub", "", "Specify client output buffer limits for clients subscribed to at least one pubsub channel or pattern. If the value specified is different that the one present on the DB, this setting will apply.")
 	distributeSubscribers := flag.Bool("oss-cluster-api-distribute-subscribers", false, "read cluster slots and distribute subscribers among them.")
 	printMessages := flag.Bool("print-messages", false, "print messages.")
-	//TODO FIX ME
-	//dialTimeout := flag.Duration("redis-timeout", time.Second*300, "determines the timeout to pass to redis connection setup. It adjust the connection, read, and write timeouts.")
+	verbose := flag.Bool("verbose", false, "verbose print.")
+	version := flag.Bool("version", false, "print version and exit.")
+	timeout := flag.Duration("redis-timeout", time.Second*30, "determines the timeout to pass to redis connection setup. It adjust the connection, read, and write timeouts.")
+	poolSizePtr := flag.Int(redisPoolSize, redisPoolSizeDefault, "determines the timeout to pass to redis connection setup. It adjust the connection, read, and write timeouts.")
 	resp := flag.Int("resp", 2, "redis command response protocol (2 - RESP 2, 3 - RESP 3)")
 	flag.Parse()
-	totalMessages = 0
-	var nodes []radix.ClusterNode
-	var nodesAddresses []string
-	var node_subscriptions_count []int
-	opts := radix.Dialer{}
-	if *password != "" {
-		opts.AuthPass = *password
-		if *username != "" {
-			opts.AuthUser = *username
-		}
+
+	git_sha := toolGitSHA1()
+	git_dirty_str := ""
+	if toolGitDirty() {
+		git_dirty_str = "-dirty"
 	}
-	if *resp == 2 {
-		opts.Protocol = "2"
-	} else if *resp == 3 {
-		opts.Protocol = "3"
+	if *version {
+		fmt.Fprintf(os.Stdout, "pubsub-sub-bench (git_sha1:%s%s)\n", git_sha, git_dirty_str)
+		os.Exit(0)
 	}
 
 	if *test_time != 0 && *messages_per_channel_subscriber != 0 {
 		log.Fatal(fmt.Errorf("--messages and --test-time are mutially exclusive ( please specify one or the other )"))
 	}
-
-	if *distributeSubscribers {
-		nodes, nodesAddresses, node_subscriptions_count = getClusterNodesFromTopology(host, port, nodes, nodesAddresses, node_subscriptions_count, opts)
-	} else {
-		nodes, nodesAddresses, node_subscriptions_count = getClusterNodesFromArgs(nodes, port, host, nodesAddresses, node_subscriptions_count)
-	}
-
-	if strings.Compare(*client_output_buffer_limit_pubsub, "") != 0 {
-		checkClientOutputBufferLimitPubSub(nodes, client_output_buffer_limit_pubsub, opts)
-	}
+	log.Println(fmt.Sprintf("pubsub-sub-bench (git_sha1:%s%s)", git_sha, git_dirty_str))
 
 	ctx := context.Background()
+	nodeCount := 0
+	poolSize := *poolSizePtr
+	var nodesAddresses []string
+	var nodeClients []*redis.Client
+	var clusterClient *redis.ClusterClient
+	var standaloneClient *redis.Client
+
+	standaloneOptions := redis.Options{Protocol: *resp,
+		Addr:         fmt.Sprintf("%s:%s", *host, *port),
+		Username:     *username,
+		Password:     *password,
+		DialTimeout:  *timeout,
+		ReadTimeout:  *timeout,
+		WriteTimeout: *timeout,
+		PoolSize:     poolSize,
+	}
+	clusterOptions := redis.ClusterOptions{Protocol: *resp,
+		Addrs:        []string{fmt.Sprintf("%s:%s", *host, *port)},
+		Username:     *username,
+		Password:     *password,
+		DialTimeout:  *timeout,
+		ReadTimeout:  *timeout,
+		WriteTimeout: *timeout,
+		PoolSize:     poolSize,
+	}
+
+	if *distributeSubscribers {
+		clusterClient = redis.NewClusterClient(&clusterOptions)
+		// ReloadState reloads cluster state. It calls ClusterSlots func
+		// to get cluster slots information.
+		log.Println("Reloading cluster state.")
+		clusterClient.ReloadState(ctx)
+		err := clusterClient.Ping(ctx).Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Getting cluster slots info.")
+		slots, err := clusterClient.ClusterSlots(ctx).Result()
+		if err != nil {
+			log.Fatal(err)
+		}
+		nodeCount = len(slots)
+		log.Println(fmt.Sprintf("Detailing cluster slots info. Total nodes: %d", nodeCount))
+		for _, slotInfo := range slots {
+			log.Println(fmt.Sprintf("\tSlot range start %d end %d. Nodes: %v", slotInfo.Start, slotInfo.End, slotInfo.Nodes))
+			nodesAddresses = append(nodesAddresses, slotInfo.Nodes[0].Addr)
+		}
+		fn := func(ctx context.Context, client *redis.Client) (err error) {
+			nodeClients = append(nodeClients, client)
+			return
+		}
+		clusterClient.ForEachMaster(ctx, fn)
+	} else {
+		nodeCount = 1
+		nodesAddresses = append(nodesAddresses, fmt.Sprintf("%s:%s", *host, *port))
+		standaloneClient = redis.NewClient(&standaloneOptions)
+		err := standaloneClient.Ping(ctx).Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+		nodeClients = append(nodeClients, standaloneClient)
+	}
+	//
+	if strings.Compare(*client_output_buffer_limit_pubsub, "") != 0 {
+		log.Println("client-output-buffer-limit-pubsub is not being enforced currently.")
+	}
+
+	totalMessages = 0
+	total_channels := *channel_maximum - *channel_minimum + 1
+	total_subscriptions := total_channels * *subscribers_per_channel
+	total_messages := int64(total_subscriptions) * *messages_per_channel_subscriber
+	subscriptions_per_node := total_subscriptions / nodeCount
+
+	log.Println(fmt.Sprintf("Total subcriptions: %d. Subscriptions per node %d. Total messages: %d", total_subscriptions, subscriptions_per_node, total_messages))
+	log.Println(fmt.Sprintf("Will use a subscriber prefix of: %s<channel id>", *subscribe_prefix))
+
+	if *poolSizePtr == 0 {
+		poolSize = subscriptions_per_node
+		log.Println(fmt.Sprintf("Setting per Node pool size of %d given you haven't specified a value and we have %d Subscriptions per node. You can control this option via --%s=<value>", poolSize, subscriptions_per_node, redisPoolSize))
+		clusterOptions.PoolSize = poolSize
+		log.Println("Reloading cluster state given we've changed pool size.")
+		clusterClient = redis.NewClusterClient(&clusterOptions)
+		// ReloadState reloads cluster state. It calls ClusterSlots func
+		// to get cluster slots information.
+		clusterClient.ReloadState(ctx)
+		err := clusterClient.Ping(ctx).Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 	// trap Ctrl+C and call cancel on the context
 	// We Use this instead of the previous stopChannel + chan radix.PubSubMessage
 	ctx, cancel := context.WithCancel(ctx)
@@ -154,11 +234,7 @@ func main() {
 
 	// a WaitGroup for the goroutines to tell us they've stopped
 	wg := sync.WaitGroup{}
-	total_channels := *channel_maximum - *channel_minimum + 1
-	subscriptions_per_node := total_channels / len(nodes)
-	total_subscriptions := total_channels * *subscribers_per_channel
-	total_messages := int64(total_subscriptions) * *messages_per_channel_subscriber
-	fmt.Println(fmt.Sprintf("Total subcriptions: %d. Subscriptions per node %d. Total messages: %d", total_subscriptions, subscriptions_per_node, total_messages))
+
 	if *cpuprofile != "" {
 		f, err := os.Create(*cpuprofile)
 		if err != nil {
@@ -166,17 +242,39 @@ func main() {
 		}
 		pprof.StartCPUProfile(f)
 	}
-
+	totalCreatedClients := 0
 	if strings.Compare(*subscribers_placement, "dense") == 0 {
 		for channel_id := *channel_minimum; channel_id <= *channel_maximum; channel_id++ {
+			channel := fmt.Sprintf("%s%d", *subscribe_prefix, channel_id)
 			for channel_subscriber_number := 1; channel_subscriber_number <= *subscribers_per_channel; channel_subscriber_number++ {
-				nodes_pos := channel_id % len(nodes)
-				node_subscriptions_count[nodes_pos]++
-				addr := nodes[nodes_pos]
-				channel := fmt.Sprintf("%s%d", *subscribe_prefix, channel_id)
+				totalCreatedClients++
 				subscriberName := fmt.Sprintf("subscriber#%d-%s%d", channel_subscriber_number, *subscribe_prefix, channel_id)
+				var client *redis.Client
+				var err error = nil
+				ctx = context.Background()
+				// In case of SSUBSCRIBE the node is associated the to the channel name
+				if strings.Compare(*mode, "ssubscribe") == 0 && *distributeSubscribers == true {
+					client, err = clusterClient.MasterForKey(ctx, channel)
+					if err != nil {
+						log.Fatal(err)
+					}
+					if *verbose {
+						log.Println(fmt.Sprintf("client %d is a CLUSTER client connected to %v. Subscriber name %s", totalCreatedClients, client.String(), subscriberName))
+					}
+				} else {
+					nodes_pos := channel_id % nodeCount
+					addr := nodesAddresses[nodes_pos]
+					client = nodeClients[nodes_pos]
+					if *verbose {
+						log.Println(fmt.Sprintf("client %d is a STANDALONE client connected to node %d (address %s). Subscriber name %s", totalCreatedClients, nodes_pos, addr, subscriberName))
+					}
+					err = client.Ping(ctx).Err()
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
 				wg.Add(1)
-				go subscriberRoutine(addr.Addr, *mode, subscriberName, channel, *printMessages, ctx, &wg, opts, *resp)
+				go subscriberRoutine(*mode, channel, *printMessages, ctx, &wg, client)
 			}
 		}
 	}
@@ -236,51 +334,6 @@ func main() {
 	wg.Wait()
 }
 
-func getClusterNodesFromArgs(nodes []radix.ClusterNode, port *string, host *string, nodesAddresses []string, node_subscriptions_count []int) ([]radix.ClusterNode, []string, []int) {
-	nodes = []radix.ClusterNode{}
-	ports := strings.Split(*port, ",")
-	for idx, nhost := range strings.Split(*host, ",") {
-		node := radix.ClusterNode{
-			Addr:            fmt.Sprintf("%s:%s", nhost, ports[idx]),
-			ID:              "",
-			Slots:           nil,
-			SecondaryOfAddr: "",
-			SecondaryOfID:   "",
-		}
-		nodes = append(nodes, node)
-		nodesAddresses = append(nodesAddresses, node.Addr)
-		node_subscriptions_count = append(node_subscriptions_count, 0)
-	}
-	return nodes, nodesAddresses, node_subscriptions_count
-}
-
-func getClusterNodesFromTopology(host *string, port *string, nodes []radix.ClusterNode, nodesAddresses []string, node_subscriptions_count []int, opts radix.Dialer) ([]radix.ClusterNode, []string, []int) {
-	// Create a normal redis connection
-	ctx := context.Background()
-	conn, err := opts.Dial(ctx, "tcp", fmt.Sprintf("%s:%s", *host, *port))
-	if err != nil {
-		panic(err)
-	}
-	var topology radix.ClusterTopo
-	err = conn.Do(ctx, radix.FlatCmd(&topology, "CLUSTER", "SLOTS"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, slot := range topology.Map() {
-		slot_host := strings.Split(slot.Addr, ":")[0]
-		slot_port := strings.Split(slot.Addr, ":")[1]
-		if strings.Compare(slot_host, "127.0.0.1") == 0 {
-			slot.Addr = fmt.Sprintf("%s:%s", *host, slot_port)
-		}
-		nodes = append(nodes, slot)
-		nodesAddresses = append(nodesAddresses, slot.Addr)
-		node_subscriptions_count = append(node_subscriptions_count, 0)
-	}
-	conn.Close()
-	return nodes, nodesAddresses, node_subscriptions_count
-}
-
 func updateCLI(tick *time.Ticker, c chan os.Signal, message_limit int64, w *tabwriter.Writer, test_time int) (bool, time.Time, time.Duration, uint64, []float64) {
 
 	start := time.Now()
@@ -327,43 +380,4 @@ func updateCLI(tick *time.Ticker, c chan os.Signal, message_limit int64, w *tabw
 		}
 	}
 	return false, start, time.Since(start), totalMessages, messageRateTs
-}
-
-func checkClientOutputBufferLimitPubSub(nodes []radix.ClusterNode, client_output_buffer_limit_pubsub *string, opts radix.Dialer) {
-	for _, slot := range nodes {
-		ctx := context.Background()
-		conn, err := opts.Dial(ctx, "tcp", slot.Addr)
-		if err != nil {
-			panic(err)
-		}
-		_, err, pubsubTopology := getPubSubBufferLimit(err, conn)
-		if strings.Compare(*client_output_buffer_limit_pubsub, pubsubTopology) != 0 {
-			fmt.Println(fmt.Sprintf("\tCHANGING DB pubsub topology for address %s from %s to %s", slot.Addr, pubsubTopology, *client_output_buffer_limit_pubsub))
-
-			err = conn.Do(ctx, radix.FlatCmd(nil, "CONFIG", "SET", "client-output-buffer-limit", fmt.Sprintf("pubsub %s", *client_output_buffer_limit_pubsub)))
-			if err != nil {
-				log.Fatal(err)
-			}
-			_, err, pubsubTopology = getPubSubBufferLimit(err, conn)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println(fmt.Sprintf("\tCHANGED DB pubsub topology for address %s: %s", slot.Addr, pubsubTopology))
-		} else {
-			fmt.Println(fmt.Sprintf("\tNo need to change pubsub topology for address %s: %s", slot.Addr, pubsubTopology))
-		}
-		conn.Close()
-	}
-}
-
-func getPubSubBufferLimit(err error, conn radix.Conn) ([]string, error, string) {
-	var topologyResponse []string
-	ctx := context.Background()
-	err = conn.Do(ctx, radix.FlatCmd(&topologyResponse, "CONFIG", "GET", "client-output-buffer-limit"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	i := strings.Index(topologyResponse[1], "pubsub ")
-	pubsubTopology := topologyResponse[1][i+7:]
-	return topologyResponse, err, pubsubTopology
 }
